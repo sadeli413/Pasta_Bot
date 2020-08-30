f37376bfd7a159986692b57a389044b0b1bc0506
Sadeli <txiinow@gmail.com>
2020-08-29 17:32:02 -0700

    Clean up spaghetti code in random and search commands

--- application/commands.py
+++ application/commands.py
@@ -10,7 +10,6 @@ from re import sub, search
 # custom packages
 import application.helpers.owo as Owoifier
 import application.helpers.extrapasta as Extrapasta
-import application.helpers.nsfw.randomSearch as Rs
 from application.helpers.misc import getTriggers, isCommand, timestamp
 from application.helpers.nsfw.search import Search
 from application.helpers.help import Help
@@ -63,91 +62,71 @@ class Commands:
 	# does NOT send minors
 	async def search(self, ctx, criteria):
 		print(ctx.message.content, end = " ... ")
-		"""
-		# search only works in DMChannels and NSFW channels
-		if (not isinstance(ctx.channel, discord.DMChannel)):
-			if not ctx.channel.is_nsfw():
-				await ctx.send("```css\n.search only works in nsfw channels```")
-				return
-		"""
-		
+	
 		# remove special characters
-		info = criteria.lower()
-		info = self.sanitize(info)
-		if len(info) < 1 or not search("[A-Za-z0-9]", info):
+		find = Search(criteria)
+		aq = find.AQ
+		if len(aq) < 1:
 			await ctx.send("```css\n.search [amount] {search criteria}\nGet some .help```")
 			return
 
-		# get amount and criteria
-		aAndC = self.amountAndCriteria(info)
-		amount = aAndC["amount"]
-		info = aAndC["criteria"]
-
 		# search the criteria
-		if amount > 0:
+		if aq["amount"] > 0:
 			# kinkshame if searched for lolis
-			if self.isKinkshame(ctx, info):
+			if self.isKinkshame(ctx, aq["query"]):
 				await ctx.send(Extrapasta.fbiOpenUp())
 				return
 			
-			await ctx.send("Searching{amount} for `{info}`...".format(amount = " x" + str(amount) if amount > 1 else "", info=info))
-			# send search normally
-			find = Search(info)
-			embeds = find.getMultiSauce(amount)
-			if len(embeds) > 0:
-				for embed in embeds:
-					await ctx.send(embed=embed)
-			else:
-				print("found none")
-				await ctx.send("Found no `{info}`".format(info=info))
+			await ctx.send("Searching{amount} for `{query}`...".format(amount = " x" + str(aq["amount"]) if aq["amount"] > 1 else "", query=aq["query"]))
+			await find.withArgs(ctx, "notrandom")
+			
 			timestamp()
 	
 	# searches top 25 most popular and gives a random one, or an amount
 	async def random(self, ctx, criteria):
 		print(ctx.message.content, end = " ... ")
-		"""
-		# only works in DMChannels and NSFW channels
-		if (not isinstance(ctx.channel, discord.DMChannel)):
-			if not ctx.channel.is_nsfw():
-				await ctx.send("```css\n.random only works in nsfw channels```")
-				return
-		"""	
+	
 		# no arguments
 		if len(criteria) < 1:
 			await ctx.send("Fetching random sauce...")
-			await Rs.noArgs(ctx)
+			await Search.noArgs(ctx)
 			timestamp()
 			return
 		
 		# remove special characters
-		info = criteria.lower()
-		info = self.sanitize(info)
-		if not search("[A-Za-z0-9]", info):
+		find = Search(criteria)
+		aq = find.AQ
+		if len(aq) < 1:
 			await ctx.send("```css\n.random [amount] [search criteria]\nGet some .help```")
 			return
 		
 		# a single number argument
-		if info.strip().isnumeric():
-			amount = abs(int(info))
+		if aq["sanitized"].isnumeric():
+			amount = abs(int(aq["sanitized"]))
 			if amount > 0:
 				await ctx.send("Fetching random sauce{amount}...".format(amount=" x"+str(amount) if amount > 1 else ""))
 				for i in range(amount):
-					await Rs.noArgs(ctx)
-		# [amount] [criteria] args
+					await Search.noArgs(ctx)
+
 		else:
-			aAndC = self.amountAndCriteria(info)
-			amount = aAndC["amount"]
-			info = aAndC["criteria"]
-			
-			if amount > 0:
+			# [amount] [criteria] args
+			if aq["amount"] > 0:
 				# kink shame if searched for minors
-				if self.isKinkshame(ctx, info):
+				if self.isKinkshame(ctx, aq["query"]):
 					await ctx.send(Extrapasta.fbiOpenUp())
 					return
 						
-				await ctx.send("Random search{amount}{extra}...".format(amount=" x" + str(amount) if amount > 1 else "", extra =" for `" + info + "`" if len(info) > 0 else ""))
-				await Rs.yesArgs(ctx, amount, info)
+				await ctx.send("Random search{amount}{query}...".format(amount=" x" + str(aq["amount"]) if aq["amount"] > 1 else "", query =" for `" + aq["query"] + "`" if len(aq["query"]) > 0 else ""))
+				await find.withArgs(ctx, "random")
 		timestamp()
+
+	# check if the user searched for loli or shota
+	def isKinkshame(self, ctx, info):
+		# ensure that removal tags like -tag:loli does NOT get kinkshamed
+		for arg in info.split(" "):
+			if ("loli" in arg or "shota" in arg) and not arg[0].startswith("-"):
+				return True
+		return False
 					
 	# owoify member messages
 	async def owo(self, ctx, *members : discord.Member):
@@ -183,9 +162,6 @@ class Commands:
 		err = "An error has occured at Guild **{guild}** in Channel **{channel}** from:```css\n{message}```"
 		await OWNER.send(err.format(guild=guild, channel=channel, message=ctx.message.content))
 
-	"""
-	The rest are helper funcitons
-	"""
 	# of the past 200 messages, delete those sent by Pasta_Bot
 	async def clean(self, ctx):
 		deleted = await ctx.channel.purge(limit = 200, check=self.isBot)
@@ -193,39 +169,3 @@ class Commands:
 	
 	def isBot(self, message):
 		return message.author == self.client.user
-	
-	# info is already lowered and sanitized
-	def amountAndCriteria(self, info):
-		# get amount
-		crit = info.strip()
-		args = crit.split(" ")
-		first = args[0]
-		# if there's only a number search that or If there's no number, amount is 1
-		if crit.isnumeric() or not first.isnumeric():
-			amount = 1
-		# set amount, and remove it from info
-		else:
-			amount = abs(int(first))
-			args.pop(0)
-			crit = "".join(i + " " for i in args)[:-1] # delete the space at the end
-		return {
-			"amount":amount,
-			"criteria":crit
-		}
-	
-	# check if the user searched for loli or shota
-	def isKinkshame(self, ctx, info):
-		# ensure that removal tags like -tag:loli does NOT get kinkshamed
-		for arg in info.split(" "):
-			if ("loli" in arg or "shota" in arg) and not arg[0].startswith("-"):
-				return True
-		return False
-		
-	# remove multiple whitespace and special characters besides :"\s
-	def sanitize(self, word):
-		new = sub("\s+", " ", word)
-		new = sub(":+", ":", new)
-		new = sub("\"+", "\"", new)
-		new = sub("-+", "-", new)
-		new = sub("[^A-Z^a-z^0-9^\-^:^\"^\s]", "", new)
-		return new
--- application/helpers/nsfw/search.py
+++ application/helpers/nsfw/search.py
@@ -5,41 +5,139 @@ Categories: tags, artists, characters, parodies
 """
 
 import requests
+from re import sub, search
+from random import randint, choice
 # custom package
 from application.helpers.nsfw.sauce import Sauce
+
+
 
 class Search:
 	def __init__(self, criteria):
 		# search url
-		self.criteria = criteria.replace(" ", "+").replace(":", "%3A" )
-		self.url = "https://nhentai.net//api/galleries/search?query={criteria}&sort=popular".format(criteria=self.criteria)
-		self.response = requests.get(self.url)
-		if self.doesExist():
-			self.data = self.response.json()
+		self.criteria = criteria
+		self.AQ = self.amountAndQuery()
+		if len(self.AQ) > 0:
+			self.url = "https://nhentai.net//api/galleries/search?query={query}&sort=popular".format(query=self.AQ["query"])
+			self.response = requests.get(self.url)
+			if self.doesExist():
+				self.data = self.response.json()
+			else:
+				self.data = None
 	
 	def doesExist(self):
 		return self.response.status_code == 200
 	
+	# get amount sauce of a search criteria
+	async def withArgs(self, ctx, flag):
+		if flag == "random":
+			embeds = self.getRandSauce(self.getNumbers()) # get amount number of numbers given by criteria
+		else:
+			embeds = self.getMultiSauce()
+		# make sure the embed will be valid
+		if len(embeds) > 0:
+			for embed in embeds:
+				await ctx.send(embed=embed)
+		else:
+			await ctx.send("Could not find {query}".format(query=self.AQ["query"]))
+
 	# maximum 25
-	def getMultiSauce(self, amount):
-		num = amount
+	def getMultiSauce(self):
+		amount = self.AQ["amount"]
 		embeds = []
 		numbers = self.getNumbers()
-		if num > 0:
-			# make sure num is not greater than len(numbers)
-			if num > len(numbers):
-				num = len(numbers)
+		if amount > 0:
+			# make sure namount um is not greater than len(numbers)
+			if amount > len(numbers):
+				amount = len(numbers)
 			# get num amount of sauces
 			if len(numbers) > 0:
-				for i in range(num):
+				for i in range(amount):
 					print(numbers[i], end = " ")
 					embeds.append(Sauce(numbers[i]).getEmbed())
 				print()
 		return embeds
+
+	def getRandSauce(self, numbers):
+		amount = self.AQ["amount"]
+		embeds = []
+		if amount > 0:
+			# make sure num is not greater than how many numbers there are
+			if amount > len(numbers):
+				amount = len(numbers)
+			# get random sauces: limit is len(numbers) and num amount
+			i = 0
+			while len(numbers) > 0 and i < amount:
+				# get a random sauce from numbers
+				rand = choice(numbers)
+				print(rand, end = " ")
+				sauce = Sauce(rand)
+				embeds.append(sauce.getEmbed())
+				# make sure there are no duplicates
+				numbers.remove(rand)
+				i += 1
+			print()
+		return embeds
 		
-	# get all "numbers" on this page
+	# get all "numbers" on the search page
 	def getNumbers(self):
 		numbers = []
-		for key in self.data["result"]:
-			numbers.append(str(key["id"])) # id is something like 177013
+		if self.data is not None:
+			for key in self.data["result"]:
+				numbers.append(str(key["id"])) # id is something like 177013
 		return numbers
+
+	# [amount] {query}
+	def amountAndQuery(self):
+		#sanitize query
+		sanitized = self.sanitize(self.criteria)
+		if not search("[A-Za-z0-9]", sanitized):
+			return {}
+
+		query = sanitized
+		# get amount
+		args = query.split(" ")
+		first = args[0]
+		# if there's only a number search that or if there's no number, amount is 1
+		if query.isnumeric() or not first.isnumeric():
+			amount = 1
+		else:
+			# set amount and remove the amount from query
+			amount = abs(int(first))
+			args.pop(0)
+			query = "".join(i + " " for i in args)[:-1] # delete the final space
+			# url sanitizer
+			query = query.replace(" ", "+").replace(":", "%3A" )
+		return {
+			"sanitized": sanitized,
+			"amount": amount,
+			"query": query
+		}
+
+	# remove multiple whitespace and special characters besides :"\s
+	def sanitize(self, word):
+		new = word.lower()
+		new = sub("\s+", " ", new)
+		new = sub(":+", ":", new)
+		new = sub("\"+", "\"", new)
+		new = sub("-+", "-", new)
+		new = sub("[^A-Za-z0-9\-:\"\s]", "", new)
+		new = new.strip()
+		return new
+
+	# get one random sauce
+	@staticmethod
+	async def noArgs(ctx):
+		rand = randint(10000, 999999)
+		print(rand, end = " ")
+		sauce = Sauce(str(rand))
+		# make sure it exists
+		while (not sauce.doesExist()):
+			print(rand, end = " ")
+			rand = randint(10000, 999999)
+			sauce = Sauce(str(rand))
+		
+		print("GAVE: " + sauce.number)
+		await ctx.send(embed=sauce.getEmbed())
+
+	
